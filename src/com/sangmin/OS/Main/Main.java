2add41fe127cfa7a20963328538c32413837af3a
Park sangmin <smpsmp0509@gmail.com>
2017-10-13 18:50:25 +0900

    Main class complete

--- src/com/sangmin/OS/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sangmin.OS.Main;
-
-public class Main {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
---
+++ src/com/sangmin/OS/main/Main.java
@@ -0,0 +1,10 @@
+package com.sangmin.OS.main;
+
+import com.sangmin.OS.operatingsystem.OperatingSystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		OperatingSystem os = new OperatingSystem();
+	}
+}
