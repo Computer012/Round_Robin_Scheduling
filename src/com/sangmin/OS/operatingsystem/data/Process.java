521ad875fb77709dbd13c8b3ed3cf5c13ece3ae8
Park sangmin <smpsmp0509@gmail.com>
2017-10-13 18:50:14 +0900

    Process Class complete

--- src/com/sangmin/OS/operatingsystem/data/Process.java
+++ src/com/sangmin/OS/operatingsystem/data/Process.java
@@ -1,5 +1,38 @@
 package com.sangmin.OS.operatingsystem.data;
 
 public class Process {
+	String processName = null;
+	int burstTime = 0;
+	int watingTime = 0;
+	int dealingTime = 0;
 
+	public Process(String processName, int burstTime) {
+		this.processName = processName;
+		this.burstTime = burstTime;
+	}
+
+	public int getWatingTime() {
+		return watingTime;
+	}
+
+	public void setWatingTime(int watingTime) {
+		this.watingTime = watingTime;
+	}
+
+	public int getDealingTime() {
+		return dealingTime;
+	}
+
+	public void setDealingTime(int dealingTime) {
+		this.dealingTime = dealingTime;
+	}
+
+	public String getProcessName() {
+		return processName;
+	}
+
+	public int getBurstTime() {
+		return burstTime;
+	}
+	
 }
